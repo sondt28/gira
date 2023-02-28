d0309d87187033bd2064e4698eca2ac844ac97ea
son <son286202@gmail.com>
2023-02-28 10:07:39 +0700

    update user model

--- src/main/java/com/son/gira/user/model/User.java
+++ src/main/java/com/son/gira/user/model/User.java
@@ -1,11 +1,18 @@
 package com.son.gira.user.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
+
+import com.son.gira.role.model.GroupRole;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -26,4 +33,10 @@ public class User {
 	
 	@Column(name = "password")
 	private String password;
+	
+	@ManyToMany
+	@JoinTable(name = "user_group_role", 
+				joinColumns = @JoinColumn(name = "user_id"),
+				inverseJoinColumns = @JoinColumn(name = "group_role_id"))	
+	private List<GroupRole> groupRoles;
 }
